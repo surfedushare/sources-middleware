fae8ec832704fdc8e50c7873147b4c5714c91376
Fako Berkers <email@fakoberkers.nl>
2022-02-22 15:35:09 +0100

    Adds the entity endpoint with mocks for persons

---
+++ api/core/mock/persons.py
@@ -0,0 +1,42 @@
+import os
+import json
+from hashlib import md5
+
+from django.utils.text import slugify
+
+
+class PersonsMock(object):
+
+    def __init__(self):
+        with open(os.path.join("core", "mock", "fixtures", "persons.json"), encoding="utf-8") as fixtures_json:
+            data = json.load(fixtures_json)
+        self.data = [
+            self.build_person(person["value"], bool(ix % 2), not ix % 2)
+            for ix, person in enumerate(data)
+        ]
+
+    def build_person(self, name, include_email=True, include_orcid=False):
+        hash = md5()
+        hash.update(name.encode("utf-8"))
+        external_id = hash.hexdigest()
+        email = f"{slugify(name)}@publinova-mock.nl" if include_email else None
+        orcid = external_id if include_orcid else None
+        return {
+            "name": name,
+            "external_id": external_id,
+            "email": email,
+            "orcid": orcid,
+            "isni": None,
+            "dai": None,
+            "first_name": None,
+            "last_name": None,
+            "prefix": None,
+            "initials": None,
+            "title": None,
+            "phone": None,
+            "skills": [],
+            "theme": None,
+            "description": None,
+            "parties": [],
+            "photo_url": None
+        }
--- api/core/views/entities.py
+++ api/core/views/entities.py
@@ -1,19 +1,92 @@
-from django.shortcuts import get_object_or_404
-from rest_framework import views
-from rest_framework.status import HTTP_200_OK, HTTP_422_UNPROCESSABLE_ENTITY
-from rest_framework.response import Response
+from urllib.parse import urlparse, parse_qs, urlunparse
 
+from django.shortcuts import get_object_or_404
+from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
+from rest_framework import views
+from rest_framework.status import HTTP_422_UNPROCESSABLE_ENTITY
+from rest_framework.response import Response
+from rest_framework.serializers import ValidationError as APIValidationError
+from rest_framework.pagination import PageNumberPagination
+
+from api.schema import MiddlewareAPISchema
 from core.models import Source
+from core.mock.persons import PersonsMock
+
+
+MOCKS = {
+    "persons": PersonsMock()
+}
 
 
 class ListEntities(views.APIView):
     """
-    TODO: Documentation string ...
+    This endpoint returns objects of the specified entity for the specified source.
+    Values for the *source* and *entity* parameters can be acquired from the sources endpoint.
+    If you encounter "unprocessable entity" errors
+    be sure to check the sources endpoint whether the requested source supports the requested entity.
+
+    ## Response body
+
+    **count**: The total amount of entities available from the source
+
+    **next**: A link to the next page of results
+
+    **previous**: A link to the previous page of results, this property may always be null for some sources
+
+    **results**: A list of objects representing the entities. Properties vary depending on requested entity.
     """
 
+    schema = MiddlewareAPISchema()
+
+    @staticmethod
+    def _validate_cursor(cursor):
+        cursor_validator = RegexValidator("page\|\d\|\d")
+        try:
+            cursor_validator(cursor)
+        except ValidationError:
+            raise APIValidationError(detail="Invalid cursor")
+
+    @staticmethod
+    def _convert_to_cursor_response(response, page_size):
+        for link_key in ["next", "previous"]:
+            if not response.data[link_key]:
+                continue
+            link = urlparse(response.data[link_key])
+            params = parse_qs(link.query)
+            link_page_param = params.get("page", [])
+            link_page_number = link_page_param[0] if len(link_page_param) else 1
+            link = link._replace(query=f"cursor=page|{link_page_number}|{page_size}")
+            response.data[link_key] = urlunparse(link)
+        return response
+
+    def _get_paginated_response(self, request, cursor, data):
+        # Monkey patch the request to fool the pagination class into page pagination
+        request.GET._mutable = True
+        if not cursor.startswith("page"):
+            raise APIValidationError(detail="Invalid cursor")
+        pagination_type, page_number, page_size = cursor.split("|")
+        request.GET.update({
+            "page": page_number,
+            "page_size": page_size
+        })
+        # Render the standard paginated response
+        paginator = PageNumberPagination()
+        paginator.page_size_query_param = "page_size"
+        page_data = paginator.paginate_queryset(data, request, view=self)
+        response = paginator.get_paginated_response(data=page_data)
+        # Modify the next and previous URL's to use our own cursor format
+        return self._convert_to_cursor_response(response, page_size)
+
     def get(self, request, *args, **kwargs):
+        # Read and validate input params
+        # Pagination cursor
+        cursor = request.GET.get("cursor", "page|1|100")
+        self._validate_cursor(cursor)
+        # Path variables
         source = get_object_or_404(Source, slug=kwargs.get("source", None))
         entity = kwargs.get("entity", None)
         if not source.is_allowed(entity):
             return Response(status=HTTP_422_UNPROCESSABLE_ENTITY)
-        return Response(data=[], status=HTTP_200_OK)
+        # Return paginated results by parsing the cursor
+        return self._get_paginated_response(request, cursor, MOCKS[entity].data)
