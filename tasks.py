a19247e68c52cf939d7aa23d80905c6de559f793
Fako Berkers <email@fakoberkers.nl>
2022-05-27 08:11:16 +0200

    Adds a sync repository state command

--- tasks.py
+++ tasks.py
@@ -4,10 +4,12 @@ from environments.project import create_configuration_and_session
 from commands.postgres.invoke import setup_postgres_localhost
 from commands.deploy import (prepare_builds, build, push, deploy, migrate, print_available_images,
                              print_running_containers)
+from commands.aws.repository import sync_repository_state
 
 
 environment, _ = create_configuration_and_session(use_aws_default_profile=False)
-aws_collection = Collection("aws", build, push, migrate, print_available_images, print_running_containers, deploy)
+aws_collection = Collection("aws", build, push, migrate, print_available_images, print_running_containers, deploy,
+                            sync_repository_state)
 aws_collection.configure(environment)
 
 
