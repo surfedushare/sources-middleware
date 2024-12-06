7c2552c1aa06eefae858804fb8f3721b79373ec9
Fako Berkers <email@fakoberkers.nl>
2024-12-06 11:53:25 +0100

    Allows multiple fragments per description type in person profiles

--- api/sources/tests/extraction/test_hanze.py
+++ api/sources/tests/extraction/test_hanze.py
@@ -172,16 +172,23 @@ class TestPersonsExtraction(ExtractorTestCase):
             "Expected academic qualifications to be added at end of description as list with single newlines"
         )
         self.assertIsNone(self.results[2]["description"], "Expected None if there is no description data")
-        self.assertTrue(
-            self.results[9]["description"].startswith(
-                "Musicians' biographical learning processes, lifelong and lifewide learning\n\n"
-            ),
+        self.assertIn(
+            "innovative music practices\n\n", self.results[9]["description"],
             "Expected multiple sections to be joined by two newlines"
         )
         self.assertEqual(
-            len(self.results[9]["description"]), 9970,
+            len(self.results[9]["description"]), 10075,
             "Expected description concatenations to be long potentially"
         )
+        expected_description_fragments = [
+            "Musicians' biographical learning processes, lifelong and lifewide learning",
+            "Biographical research into professional musicians, ethnographic research into innovative music practices"
+        ]
+        for description_fragment in expected_description_fragments:
+            self.assertIn(
+                description_fragment, self.results[9]["description"],
+                "Expected descriptions with the same type to be joined together in final descrption"
+            )
 
     def test_get_isni(self):
         self.assertIsNone(
